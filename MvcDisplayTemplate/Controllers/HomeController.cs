b4cb3bb7fa4b6f6a409e9adc3c6c7a70d36e9e04
foxpro87 <sbwoo@wizsolution.net>
2013-06-05 21:02:57 +0900

    DisplayTemplate under shared folder
    DisplayTemplate under shared folder

---
+++ MvcDisplayTemplate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDisplayTemplate.Models;
+
+namespace MvcDisplayTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            User user = new User()
+            {
+                Id=101,
+                Name = "주의사신",
+                Reputation = 1234
+            };
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            User user = new User()
+            {
+                Id=905,
+                Name = "파워 개발자",
+                Reputation = 54432
+            };
+            return View(user);
+        }
+
+    }
+}
